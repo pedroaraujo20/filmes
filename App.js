b2285f11190524854de3cb86291bde53ea2b15e7
Pedro Araujo <pedro-foa@hotmail.com>
2019-05-22 12:25:46 -0300

    Criando cartaz

---
+++ App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+
+export default class cursoreact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmes:[],
+      loading:true
+    };
+    fetch('https://filmespy.herokuapp.com/api/v1/filmes')
+      .then((r)=>r.json())
+      .then((json)=>{
+        let state = this.state;
+        state.filmes = json.filmes;
+        state.loading = false;
+        this.setState(state);
+      });
+  }
+
+  render(){
+    if(this.state.loading) {
+      return (
+        <View style={[styles.container, styles.loading]}>
+          <Text style={styles.loadingText}>Carregando...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <FlatList 
+          data={this.state.filmes}
+          renderItem={({item}) => <Filme data={item}/>} 
+          keyExtractor={(item, index)=>item.titulo}       
+          />
+        </View>
+      );
+    }    
+  }
+}
+
+class Filme extends Component {
+  render() {
+    return(
+      <View style={styles.filmeArea}>
+        <Image source={{uri:this.props.data.poster.replace('http:','https:')}} style={styles.filmeImagem}/>
+        <View style={styles.filmeInfo}> 
+          <Text style={styles.filmeTitulo}>{this.props.data.titulo}</Text>
+          <Text>{this.props.data.data}</Text>
+          <Text>{this.props.data.sinopse}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop:20
+  },
+  loading:{
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  loadingTxt:{
+    fontSize: 18,
+    fontWeight:'bold'
+  },
+  filmeArea:{
+    flex:1,
+    flexDirection:'row',
+    margin:10
+  },
+  filmeImagem:{
+    width:80,
+    height:110
+  },
+  filmeInfo:{
+    flex:1,
+    flexDirection:'column',
+    marginLeft: 10
+  },
+  filmeTitulo:{
+    fontSize:18,
+    fontWeight:'bold'
+  }
+
+})
